c051f942a56aa283f50938bb885b09190a6a51fd
Ayse <aysenur_aydin@hotmail.fr>
2022-04-24 17:14:34 +0200

    Creating conflict in TestCase003 in master

--- src/hello/TestCase003.java
+++ src/hello/TestCase003.java
@@ -4,7 +4,7 @@ public class TestCase003 {
 
 
     // this is unknown  changes to feature branch
-    //that exists in master
+    //that exists in master,
     // now git will not know what to do with this
     // when we merge and conflict will happen
 
